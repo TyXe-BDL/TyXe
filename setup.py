ba2608e552ed30302ac8e59f7a609c4f1e3b3294
Hippolyt Ritter <julian.ritter.14@ucl.ac.uk>
2022-07-18 00:23:13 +0100

    fixed url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='tyxe',
     version='0.0.2',
-    url='https://github.com/karalets/TyXe',
+    url='https://github.com/TyXe-BDL/TyXe',
     author=['Hippolyt Ritter', 'Theofanis Karaletsos'],
     author_email='j.ritter@cs.ucl.ac.uk',
     description='BNNs for pytorch using pyro.',
